f92b47051c1106a2f39ca8008b396fea4eaaec2e
Steve Cherry <stevenccherry@gmail.com>
2018-05-31 06:38:33 -0400

    Add all routes

--- src/routes.js
+++ src/routes.js
@@ -10,5 +10,36 @@ export const routes = [
     moduleId: PLATFORM.moduleName('./front/front'),
     nav: true,
     title: 'Home'
+  },
+  {
+    route: [
+      'recipes'
+    ],
+    name: 'recipes',
+    moduleId: PLATFORM.moduleName('./recipes/recipes'),
+    nav: true,
+    title: 'Recipes'
+  },
+  {
+    route: [
+      'recipes/:id'
+    ],
+    name: 'recipe',
+    moduleId: PLATFORM.moduleName('./recipes/recipe')
+  },
+  {
+    route: [
+      'categories/:name'
+    ],
+    name: 'category',
+    moduleId: PLATFORM.moduleName('./categories/category')
+  },
+  {
+    route: [
+      'latest'
+    ],
+    name: 'latest',
+    moduleId: PLATFORM.moduleName('./latest/latest'),
+    title: 'Latest Recipes'
   }
 ];
